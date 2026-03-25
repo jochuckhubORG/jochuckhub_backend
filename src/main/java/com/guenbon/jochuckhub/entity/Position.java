5bae9ef84870b408e409476e015ac4b0e2dc89df
jd99iam <jidamine87593@gmail.com>
2026-03-25 21:08:35 +0900

    feat: 포지션 수정

--- src/main/java/com/guenbon/jochuckhub/entity/Position.java
+++ src/main/java/com/guenbon/jochuckhub/entity/Position.java
@@ -4,9 +4,9 @@ public enum Position {
     // 골키퍼
     GK,
     // 수비수
-    CB, LB, RB, LWB, RWB,
+    CB, LB, RB,
     // 미드필더
-    CDM, CM, CAM, LM, RM,
+    CDM, CM, CAM,
     // 공격수
-    LW, RW, ST, CF
+    LW, RW, ST;
 }
